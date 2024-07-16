1b645c23c17e88fae8c3909d0fcc0a187d24f0dc
AdityaKumarSingh101 <31300740+AdityaKumarSingh101@users.noreply.github.com>
2024-07-16 06:52:57 +0530

    - Added types to pokemon image

--- src/components/Pokemon/PokemonDetailsPage/PokemonDetails/PokemonImage.tsx
+++ src/components/Pokemon/PokemonDetailsPage/PokemonDetails/PokemonImage.tsx
@@ -1,15 +1,37 @@
+import { type_style } from "../../../../helpers/styles";
+import { ToPascalCase } from "../../../../helpers/functions";
+import { PokeAPI_Pokemon_Type } from "../../../../types/PokeAPI_DataTypes";
 export const PokemonImage = ({
-  imageURL,
   exp,
+  imageURL,
+  types,
 }: {
-  imageURL: string;
   exp: number;
+  imageURL: string;
+  types: [];
 }) => {
   return (
-    <div className="flex flex-col bg-white p-6 justify-center items-center size-80 border-black border-2 rounded-lg">
-      <img src={imageURL} />
-      <div className="bg-black text-white text-center rounded-t-md font-bold p-2 mt-2">
+    <div className="flex flex-col bg-white p-6 justify-center items-center size-80 border-black border-8 rounded-2xl">
+      {/* Base Experience */}
+      <div className="w-full text-center bg-black text-white rounded-lg mt-24 font-bold text-lg">
         Base Experience: {exp}
+      </div>
+      {/* Pokemon Image */}
+      <div>
+        <img src={imageURL} />
+      </div>
+      {/* Pokemon Types */}
+      <div className="bg-black text-white rounded-lg font-bold p-3 mb-20 w-full">
+        <span className="text-center flex flex-row gap-2 justify-evenly">
+          {types.map((type: PokeAPI_Pokemon_Type, index: number) => {
+            const style = type_style(type.type.name);
+            return (
+              <span className={style} key={index}>
+                <b>{ToPascalCase(type.type.name)}</b>
+              </span>
+            );
+          })}
+        </span>
       </div>
     </div>
   );
