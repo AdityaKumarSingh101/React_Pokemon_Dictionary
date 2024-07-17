e30ac2f66e8e85e887ba39c5372c21c338b45b90
AdityaKumarSingh101 <31300740+AdityaKumarSingh101@users.noreply.github.com>
2024-07-17 08:29:32 +0530

    Changed styling of stats card

--- src/components/Pokemon/PokemonDetailsPage/PokemonDetails/PokemonStats.tsx
+++ src/components/Pokemon/PokemonDetailsPage/PokemonDetails/PokemonStats.tsx
@@ -15,34 +15,21 @@ export const PokemonStats = ({
   return (
     <div className={PokemonDetailsCardStyle}>
       {/* Stats Title */}
-      <div className="font-bold text-xl text-center">STATS</div>
+      <div className="font-bold text-xl text-center">BASE STATS</div>
       <hr />
       {/* Stats Table Container */}
-      <div className="flex flex-col gap-2 mt-4 font-bold">
-        <div className=" flex flex-row gap-6">
-          <span className="mr-2">
-            HP : <span className="ml-2">{stats.hp}</span>
-          </span>
-          <span className="ml-2">
-            SPEED : <span className="ml-2">{stats.speed}</span>
-          </span>
-        </div>
-        <div className=" flex flex-row gap-4">
-          <span className="mr-2">
-            ATK : <span className="ml-2">{stats.atk}</span>
-          </span>
-          <span className="ml-2">
-            SP. ATK : <span className="ml-2">{stats.spAtk}</span>
-          </span>
-        </div>
-        <div className="flex flex-row gap-4">
-          <span className="mr-2">
-            DEF : <span className="ml-2">{stats.def}</span>
-          </span>
-          <span className="ml-2">
-            SP. DEF : <span className="ml-2">{stats.spDef}</span>
-          </span>
-        </div>
+      <div className="flex flex-row pt-5 items-center gap-2 font-bold">
+        <span className="pt-1">HP: {stats.hp}</span>
+        <span>|</span>
+        <span className="pt-1">SPEED: {stats.speed}</span>
+        <span>|</span>
+        <span className="pt-1">ATK: {stats.atk}</span>
+        <span>|</span>
+        <span className="pt-1">DEF: {stats.def}</span>
+        <span>|</span>
+        <span className="pt-1">SP. ATK: {stats.spAtk}</span>
+        <span>|</span>
+        <span className="pt-1">SP. DEF: {stats.spDef}</span>
       </div>
     </div>
   );
