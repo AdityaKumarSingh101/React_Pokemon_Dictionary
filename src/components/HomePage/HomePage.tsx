763f105befed15ac03061696fbeeb3703a913876
AdityaKumarSingh101 <31300740+AdityaKumarSingh101@users.noreply.github.com>
2024-07-20 20:40:04 +0530

    - Added an items option

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -6,13 +6,19 @@ export default function HomePage() {
   return (
     <>
       <div className="flex flex-col justify-center items-center">
-        <h1 className="font-bold text-3xl text-white mt-5">Poke World!</h1>
+        <h1 className="font-bold text-3xl text-white mt-5">Poke Library!</h1>
         <div className="">
           <button
             className="bg-white text-black border-black border-2 hover:bg-black hover:text-white w-25 h-15 rounded-md mt-7 ml-7 p-3 font-bold"
             onClick={() => navigate("/pokemon")}
           >
             Pokemon
+          </button>
+          <button
+            className="bg-white text-black border-black border-2 hover:bg-black hover:text-white w-25 h-15 rounded-md mt-7 ml-7 p-3 font-bold"
+            onClick={() => navigate("/item")}
+          >
+            Items
           </button>
         </div>
       </div>
