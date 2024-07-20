b11bd4f0fd2d34ab227635c438648306913a6a03
AdityaKumarSingh101 <31300740+AdityaKumarSingh101@users.noreply.github.com>
2024-07-20 20:40:57 +0530

    - Added a resuable item card component

---
+++ src/components/Items/ItemsListPage/ItemsListPageCard/ItemsListPageCard.tsx
@@ -0,0 +1,44 @@
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { PokeAPI_ItemData } from "../../../../types/PokeAPI_DataTypes";
+import ListPageCard from "../../../../atoms/ListPageCard";
+
+type ItemCardProps = {
+  name: string;
+};
+
+export default function ItemsListPageCard({ name }: ItemCardProps) {
+  const navigate = useNavigate();
+
+  const [itemData, setItemData] = useState<PokeAPI_ItemData>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const fetchitem = async () => {
+    await axios.get(`https://pokeapi.co/api/v2/item/${name}`).then((res) => {
+      const item: PokeAPI_ItemData = res.data;
+      setItemData(item);
+    });
+  };
+
+  useEffect(() => {
+    fetchitem();
+    setIsLoading(false);
+  }, []);
+  if (isLoading) {
+    return (
+      <div className="border-black border-8 rounded-md overflow-auto hover:scale-110 hover:cursor-pointer duration-500 ease-in-out hover:border-white hover:border-4">
+        Loading...
+      </div>
+    );
+  }
+  return (
+    <>
+      <ListPageCard
+        name={name}
+        imageURL={itemData?.sprites.default as string}
+        onClickNavigate={() => navigate(`/item/${name}`)}
+      />
+    </>
+  );
+}
