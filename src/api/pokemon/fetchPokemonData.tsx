2ac1465eebf372b655e458c438351b3b2980024d
Aditya <kumarsinghaditya5@gmail.com>
2024-07-22 15:02:07 +0530

    Refactored API call

--- src/api/pokemon/fetchPokemonData.tsx
+++ src/api/pokemon/fetchPokemonData.tsx
@@ -2,11 +2,15 @@ import axios from "axios";
 import type { PokeAPI_PokemonData } from "../../types/PokeAPI_DataTypes";
 
 export const fetchPokemonData = async (
-  name: string,
+  pokemonName: string,
   setPokemonData: (pokemonData: PokeAPI_PokemonData) => void
 ) => {
-  await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`).then((res) => {
+  
+  const pokemonDataURL = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+  
+  await axios.get(pokemonDataURL).then((res) => {
     const pokemon: PokeAPI_PokemonData = res.data;
     setPokemonData(pokemon);
   });
+
 };
