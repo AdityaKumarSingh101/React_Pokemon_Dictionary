5ae9e2916aec57bb6200c509a0ae02577deb31a1
Aditya <kumarsinghaditya5@gmail.com>
2024-07-22 15:06:08 +0530

    - Added routing for items list and item details page

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import PokemonListPage from "./components/Pokemon/PokemonListPage/PokemonsListPage";
-import { PokemonPage } from "./components/Pokemon/PokemonDetailsPage/PokemonDetailsPage";
+import PokemonDetailsPage from "./components/Pokemon/PokemonDetailsPage/PokemonDetailsPage";
 import ItemsListPage from "./components/Items/ItemsListPage/ItemsListPage";
+import ItemDetailsPage from "./components/Items/ItemDetailsPage/ItemDetailsPage";
 
 function App() {
   return (
@@ -12,8 +13,12 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/pokemon" element={<PokemonListPage />} />
-          <Route path="/pokemon/:pokemonName" element={<PokemonPage />} />
+          <Route
+            path="/pokemon/:pokemonName"
+            element={<PokemonDetailsPage />}
+          />
           <Route path="/item" element={<ItemsListPage />} />
+          <Route path="/item/:itemName" element={<ItemDetailsPage />} />
         </Routes>
       </BrowserRouter>
     </>
