ddcd42a0aed6e0ce4cf6fe700849be6bea4b23fa
Aditya <kumarsinghaditya5@gmail.com>
2024-07-24 08:17:29 +0530

    Changed app title font with custom font

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -6,19 +6,22 @@ export default function HomePage() {
   return (
     <>
       <div className="flex flex-col justify-center items-center">
-        <h1 className="font-bold text-3xl text-white mt-5">Poke Library!</h1>
-        <div className="">
+        <h1 className="text-9xl font-bold text-white text-center mt-5 font-chakra">
+          Poke <br />
+          Library
+        </h1>
+        <div className="mt-10 h-52 w-full flex flex-row justify-evenly">
           <button
-            className="bg-white text-black border-black border-2 hover:bg-black hover:text-white w-25 h-15 rounded-md mt-7 ml-7 p-3 font-bold"
+            className="w-[40%] bg-white text-black border-black border-2 hover:bg-black hover:text-white w-25 h-15 rounded-md mt-7 ml-7 p-3 font-bold"
             onClick={() => navigate("/pokemon")}
           >
-            Pokemon
+            <span className="font-bold text-5xl font-chakra">Pokemon</span>
           </button>
           <button
-            className="bg-white text-black border-black border-2 hover:bg-black hover:text-white w-25 h-15 rounded-md mt-7 ml-7 p-3 font-bold"
+            className="w-[40%] bg-white text-black border-black border-2 hover:bg-black hover:text-white w-25 h-15 rounded-md mt-7 ml-7 p-3 font-bold"
             onClick={() => navigate("/item")}
           >
-            Items
+            <span className="font-bold text-5xl font-chakra">Items</span>
           </button>
         </div>
       </div>
